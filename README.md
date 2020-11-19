b6edc73478ab2b3f325ade38f43ad09becc02456
Luis Santos <luis.santos@pravaler.com.br>
2020-11-19 12:06:08 -0300

    subforms

---
+++ README.md
@@ -0,0 +1,25 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+# Subforms example
+
+This repository has been created as a collection of use-cases where subforms
+can help.
+
+The application has the following use-cases:
+
+- sign up
+- logged user checkout
+- anonymous user checkout
+
+All those forms share some kind of logic and a lot of fields.
+Thanks to subforms, you can keep your code DRY and maintain it more easily.
+
+You can read more about the concept of subforms here: [https://dev.to/nicholasperetti/the-concept-of-subforms-with-react-and-formik-55mh](https://dev.to/nicholasperetti/the-concept-of-subforms-with-react-and-formik-55mh)
+
+# Contributions
+
+This repo serve as a guideline on how you should hanlde subforms.
+Anyhow, all the ideas and methodologies described in the article and used in the
+codebase are **not final**.
+
+If you have an idea on how to improve the process of handling forms, please submit a PR.
---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { BrowserRouter, Route } from "react-router-dom";
+import RouteHome from "./components/RouteHome";
+import RouteSignUp from "./components/RouteSignUp";
+import RouteCheckout from "./components/RouteCheckout";
+import RouteAnonymousCheckout from "./components/RouteAnonymousCheckout";
+import Layout from "./components/Layout";
+import { HOME_ROUTE, SIGN_UP, CHECKOUT, ANON_CHECKOUT } from "./constants";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: `#FFA500`
+    },
+    secondary: {
+      main: "#007aa7"
+    }
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <BrowserRouter>
+          <Layout>
+            <Route path={HOME_ROUTE} exact component={RouteHome} />
+            <Route path={SIGN_UP} exact component={RouteSignUp} />
+            <Route path={CHECKOUT} exact component={RouteCheckout} />
+            <Route
+              path={ANON_CHECKOUT}
+              exact
+              component={RouteAnonymousCheckout}
+            />
+          </Layout>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
